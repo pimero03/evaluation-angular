de7b7cf15397f544f13e38a1125280bdff0bf207
Melien Pierre <mrolpierre@gmail.com>
2021-05-02 20:00:30 +0200

    first commit

---
+++ src/app/formulaire/formulaire.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl,Validators ,FormBuilder} from '@angular/forms'
+
+interface  Utilisateur{
+  nom: string;
+  prenom: string;
+  email: string;
+  age: number;
+}
+
+@Component({
+  selector: 'app-formulaire',
+  templateUrl: './formulaire.component.html',
+  styleUrls: ['./formulaire.component.css']
+})
+
+export class FormulaireComponent implements OnInit {
+
+
+
+  constructor(private fb: FormBuilder) { }
+
+  formulaire = this.fb.group({
+    nom:  ['',Validators.required,Validators.minLength(3),Validators.maxLength(10),],
+
+    prenom: ['',Validators.required,Validators.minLength(3),Validators.maxLength(15),Validators.pattern('[a-zA-Z ]*')],
+
+    email: ['',Validators.required,Validators.email],
+
+    age: ['',Validators.required,Validators.min(1),Validators.max(120)]
+  })
+
+
+ get(){return this.formulaire.controls.value.get('nom')}
+
+  ngOnInit(): void {
+  }
+
+
+  onSubmit(): void{
+
+  }
+
+
+
+
+
+}
